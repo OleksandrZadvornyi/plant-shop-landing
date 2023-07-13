629d36d0a0bdfefe52eb6a2478ba549b5d5bfbba
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-07-13 17:49:21 +0300

    add styles for dropdown list in main section

---
+++ script.js
@@ -0,0 +1,18 @@
+function openDropdown() {
+  const dropdownContent = document.getElementById("dropdown-content");
+  const dropdownDisplay = dropdownContent.style.display;
+
+  if (dropdownDisplay === "none") {
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
+}
+
+function changeBtnName(element) {
+  const button = document.getElementsByClassName("dropbtn")[0];
+  button.innerHTML =
+    element.innerHTML +
+    '<img src="/images/chevron-down.svg" alt="Chevron Down" />';
+  openDropdown();
+}
